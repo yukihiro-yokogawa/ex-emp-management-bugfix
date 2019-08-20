7e60d45fb50cd4d2db2d4bb80d3a76be8f9da5e2
yukihiro-yokogawa <yukihiro.yokogawa@rakus-partners.co.jp>
2019-08-20 14:19:39 +0900

    (2-1) 改修 Administratorオブジェクトの式を短縮化 無駄な行を削除

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -28,7 +28,7 @@ public class AdministratorController {
 
 	@Autowired
 	private AdministratorService administratorService;
-	
+
 	@Autowired
 	private HttpSession session;
 
@@ -41,8 +41,8 @@ public class AdministratorController {
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdministratorForm();
 	}
-	
-	//  (SpringSecurityに任せるためコメントアウトしました)
+
+	// (SpringSecurityに任せるためコメントアウトしました)
 	@ModelAttribute
 	public LoginForm setUpLoginForm() {
 		return new LoginForm();
@@ -64,33 +64,30 @@ public class AdministratorController {
 	/**
 	 * 管理者情報を登録します.
 	 * 
-	 * @param form
-	 *            管理者情報用フォーム
+	 * @param form 管理者情報用フォーム
 	 * @return ログイン画面へリダイレクト
 	 */
 	@RequestMapping("/insert")
-	public String insert(@Validated InsertAdministratorForm form, BindingResult result,Model model) {
-		
-		if(result.hasErrors()) {
+	public String insert(@Validated InsertAdministratorForm form, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
 			return toInsert();
 		}
-			
-		//登録済みのメールアドレスかどうかの確認
-		Administrator administratorCheck = new Administrator();
-		administratorCheck = administratorService.serchByMailAddress(form.getMailAddress());
 
-		if(form.getMailAddress().equals(administratorCheck.getMailAddress())) {
-			model.addAttribute("errorMessage","既に登録されているメールアドレスです");
+		// 登録済みのメールアドレスかどうかの確認
+		Administrator administratorCheck = administratorService.serchByMailAddress(form.getMailAddress());
+
+		if (form.getMailAddress().equals(administratorCheck.getMailAddress())) {
+			model.addAttribute("errorMessage", "既に登録されているメールアドレスです");
 			return toInsert();
 		}
 
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
-		
+
 		administratorService.insert(administrator);
-		
-		
+
 		return "redirect:/";
 	}
 
@@ -110,10 +107,8 @@ public class AdministratorController {
 	/**
 	 * ログインします.
 	 * 
-	 * @param form
-	 *            管理者情報用フォーム
-	 * @param result
-	 *            エラー情報格納用オブッジェクト
+	 * @param form   管理者情報用フォーム
+	 * @param result エラー情報格納用オブッジェクト
 	 * @return ログイン後の従業員一覧画面
 	 */
 	@RequestMapping("/login")
@@ -123,10 +118,10 @@ public class AdministratorController {
 			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
-		session.setAttribute("administratorName",administrator.getName());
+		session.setAttribute("administratorName", administrator.getName());
 		return "forward:/employee/showList";
 	}
-	
+
 	/////////////////////////////////////////////////////
 	// ユースケース：ログアウトをする
 	/////////////////////////////////////////////////////
@@ -140,5 +135,5 @@ public class AdministratorController {
 		session.invalidate();
 		return "redirect:/";
 	}
-	
+
 }
